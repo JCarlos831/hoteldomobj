d1c39dfe434d8fec1245a1bffca9f03fd3286dab
Juan Montoya <jcmontoya@gmail.com>
2017-11-10 01:45:23 +0000

    fix js file

--- objects.js
+++ objects.js
@@ -1,6 +1,17 @@
-hotel
-{ name: 'Biltmore',
+var hotel = {
+  name: 'Biltmore',
   rooms: 500,
   roomsBooked: 150,
   roomService: true,
-  pool: false }
+  pool: false,
+
+  roomsAvail: function() {
+    return this.rooms - this.roomsBooked;
+  }
+};
+
+function hotelInfo() {
+  document.getElementById("name").innerHTML = hotel.name;
+  document.getElementById("rooms").innerHTML = hotel.rooms;
+  document.getElementById("roomsAvail").innerHTML = hotel.roomsAvail();
+}
